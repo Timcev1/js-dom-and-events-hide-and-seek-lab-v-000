afa8e7a51e32039d6003f8f7a86d4921da8e6b45
Timothy Cevallos <timcev@gmail.com>
2017-10-06 23:49:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selectors){
+  document.getElementById(selectors)
+}
+
+function nestedTarget(){
+  
+}
+
+function increaseRankBy(){
+  
+}
+
+function deepestChild(){
+  
+}
