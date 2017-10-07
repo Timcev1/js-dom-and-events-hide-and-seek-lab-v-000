e5c58e473ab90768b008e250912ff18a31cad940
Timothy Cevallos <timcev@gmail.com>
2017-10-07 00:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  let ranked = document.querySelectorAll(`.ranked-list`)
-    for (let i = 0, l = ranked.length; i < l; i++){
-      let child = ranked[i].child
-      for (let j = 0, c = child.length; j < c; j++) {
-        child[j].innerHTML = parseInt(child[j].innerHTML) + n
-      }
-    }
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+for (let i = 0, l = rankedLists.length; i < l; i++) {
+  let children = rankedLists[i].children
+
+  for (let j = 0, k = children.length; j < k; j++) {
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  }
+}
 }
 
 function deepestChild(){
