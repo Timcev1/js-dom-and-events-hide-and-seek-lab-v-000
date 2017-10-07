d02d8290b15d553505f1b1954d851cb17f7e9736
Timothy Cevallos <timcev@gmail.com>
2017-10-07 00:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   let ranked = document.querySelectorAll('.ranked-list')
     for (let i = 0, l = ranked.length; i < l; i++){
-      let child = ranked[i].child
-      for (let j = 0, c = child.length; j < c; j++) {
-        child[j].innerHTML = parseInt(child[j].innerHTML) + n
-      }
+      ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n;
     }
 }
 
