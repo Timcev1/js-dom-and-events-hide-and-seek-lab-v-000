8189e8711b1083ae44f111303808bd3fad3df57b
Timothy Cevallos <timcev@gmail.com>
2017-10-07 00:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-
+  let ranked = document.querySelectorAll(`.ranked-list`)
+    for (let i = 0, 1 = ranked.length; i < 1; i++){
+      let child = ranked[i].child
+      for (let j = 0, c = child.length; j < k; j++) {
+        child[j].innerHTML = parseInt(child[j].innerHTML) + n
+      }
+    }
 }
 
 function deepestChild(){
