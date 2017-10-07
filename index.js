0c798afa6e2d5954b89058375db374969e1cf72a
Timothy Cevallos <timcev@gmail.com>
2017-10-07 00:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   let ranked = document.querySelectorAll(`.ranked-list`)
-    for (let i = 0, l = ranked.length; i < 1; i++){
+    for (let i = 0, l = ranked.length; i < l; i++){
       let child = ranked[i].child
       for (let j = 0, c = child.length; j < k; j++) {
         child[j].innerHTML = parseInt(child[j].innerHTML) + n
