3d8415772922fec7b3c57005916ebd6f6f3ab77b
Timothy Cevallos <timcev@gmail.com>
2017-10-07 00:37:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selectors){
+  return document.querySelector(selectors)
+}
+
+function nestedTarget(){
+  return document.querySelector(`#nested .target`)
+}
+
+function increaseRankBy(n){
+  let ranked = document.querySelectorAll('.ranked-list')
+    for (let i = 0, l = ranked.length; i < l; i++){
+      ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n;
+    }
+}
+
+function deepestChild(){
+  let start = document.getElementById('grand-node')
+  let child = start.querySelector('div')
+  while (child){
+    start = child
+    child = start.querySelector('div')
+  }
+  return start
+}
